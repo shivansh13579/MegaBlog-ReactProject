d66a7c95936e0a864bb39694202d837a6fbaf7ec
shivansh13579 <shivansh0975@gmail.com>
2023-09-25 22:01:26 +0530

    crate auth by appwrite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+  <>
+  <h1>
+  we create the blog app.
+ </h1>
+  </>
+  )
+}
+
+export default App
