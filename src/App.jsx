8e51ddedb9372bd55b856ad9958869425ee9851c
shivansh13579 <shivansh0975@gmail.com>
2023-09-27 16:11:53 +0530

    add react-router provider in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
 import './App.css'
+import React, { useState,useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login,logout } from './store/authSlice.js';
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading,setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-  <>
-  <h1>
-  we create the blog app.
- </h1>
-  </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+          dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='w-full block'>
+        <Header/>
+        <main>
+       Todo : {/* <Outlet/> */}
+        </main>
+       <Footer/>
+    </div>
+    </div>
+  ) : null    
 }
 
 export default App
