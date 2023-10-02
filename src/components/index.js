7157f124118f92558618c2727edb4e3326f0f7f4
shivansh13579 <shivansh0975@gmail.com>
2023-10-02 22:03:22 +0530

    crate route

--- src/components/index.js
+++ src/components/index.js
@@ -11,6 +11,7 @@ import Login from "./Login";
 import Post from "./Post";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout"
 
 export {
     Header,
@@ -26,4 +27,5 @@ export {
     Post,
     PostForm,
     PostCard,
+    AuthLayout,
 }
